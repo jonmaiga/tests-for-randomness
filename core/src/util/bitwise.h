1748275641fcc316f5c240a7ac5e80316bc90244
jonkagstrom <jon@jonkagstrom.com>
2022-10-24 20:54:28 +0200

    - Remove unused popcount (missing on clang)

--- core/src/util/bitwise.h
+++ core/src/util/bitwise.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <intrin.h>
 
 namespace mixer {
 
@@ -12,9 +11,5 @@ inline uint64_t flip_bit(uint64_t i, int bit) {
 	return i ^ (1ull << bit);
 }
 
-inline uint64_t count_bits_changed(uint64_t a, uint64_t b) {
-	const uint64_t ah_bh = a ^ b;
-	return __popcnt64(ah_bh);
-}
 
 }
