10cf25d1f13481791b4fca4dbe7cf92ac027c05f
jonmaiga <jon@jonkagstrom.com>
2022-10-30 08:00:28 +0100

    - ftest

--- core/src/statistics/t_test.h
+++ core/src/statistics/t_test.h
@@ -24,5 +24,12 @@ inline double z_test(basic_stats a, basic_stats population) {
 	return 2. * normal_cdf(-std::abs(z));
 }
 
+inline double f_test(basic_stats a, basic_stats b) {
+	const auto a_nom = a.variance > b.variance;
+	const auto f = a_nom ? a.variance / b.variance : b.variance / a.variance;
+	const auto df1 = a_nom ? a.n - 1 : b.n - 1;
+	const auto df2 = a_nom ? b.n - 1 : a.n - 1;
+	return 2. * incomplete_beta(0.5 * df2, 0.5 * df1, df2 / (df2 + df1 * f));
+}
 
 }
