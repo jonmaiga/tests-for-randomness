f3d3e1bda7e89476d9af0ecb985f5bbe28886dc3
jonmaiga <jon@jonkagstrom.com>
2022-10-31 15:43:15 +0100

    - ... and the test

--- unittest/src/statistics/distributions.cpp
+++ unittest/src/statistics/distributions.cpp
@@ -24,4 +24,15 @@ TEST(two_tailed_normal_cdf, basic) {
 	EXPECT_NEAR(two_tailed_normal_cdf(10), 0., 1e-4);
 }
 
+TEST(incomplete_beta, basic) {
+	EXPECT_NEAR(incomplete_beta(1, 1, 1), 1., 1e-4);
+	EXPECT_NEAR(incomplete_beta(0.01, 0.01, 0.5), .5, 1e-4);
+	EXPECT_NEAR(incomplete_beta(0.01, 0.02, 0.5), .6667, 1e-4);
+	EXPECT_NEAR(incomplete_beta(1, 3, 0.4), 0.784, 1e-4);
+	EXPECT_NEAR(incomplete_beta(10, 12, 0.5), .6681, 1e-4);
+	EXPECT_NEAR(incomplete_beta(10, 12, 0.99), 1, 1e-4);
+	EXPECT_NEAR(incomplete_beta(12, 10, 0.99), 1., 1e-4);
+	EXPECT_NEAR(incomplete_beta(900, 1024, 0.23), 0, 1e-4);
+}
+
 }
