d1f110b640fa8b6a22b7ac5938cd00ecd30a6056
jonmaiga <jon@jonkagstrom.com>
2022-11-02 19:20:22 +0100

    - sac no change

--- unittest/src/statistics/avalanche_test.cpp
+++ unittest/src/statistics/avalanche_test.cpp
@@ -6,8 +6,17 @@
 
 namespace mixer {
 
-TEST(avalanche, bic_matrix_no_change) {
-	const auto bit_counts = run_avalanche(10, create_counter_stream(1), mx3);
+TEST(avalanche, sac_no_change) {
+	const auto bit_counts = avalanche_generate_sac(10, create_counter_stream(1), mx3);
+	uint64_t h = 0;
+	for (const auto v : bit_counts) {
+		h += mx3(v);
+	}
+	EXPECT_EQ(h, 7766637459189417547ull);
+}
+
+TEST(avalanche, bic_no_change) {
+	const auto bit_counts = avalanche_generate_bic(10, create_counter_stream(1), mx3);
 	uint64_t h = 0;
 	for (const auto v : bit_counts) {
 		h += mx3(v);
