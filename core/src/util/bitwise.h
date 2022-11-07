906e9b62196f38812bf32022b8d265becf9b2e7a
jonkagstrom <jon@jonkagstrom.com>
2022-11-07 20:04:23 +0100

    - Added byte swap

--- core/src/util/bitwise.h
+++ core/src/util/bitwise.h
@@ -16,4 +16,15 @@ inline int bit_count(uint64_t x) {
 	return std::popcount(x);
 }
 
+inline uint64_t byte_swap(uint64_t val) {
+	return ((((val) & 0xff00000000000000ull) >> 56) |
+                    (((val) & 0x00ff000000000000ull) >> 40) |
+                    (((val) & 0x0000ff0000000000ull) >> 24) |
+                    (((val) & 0x000000ff00000000ull) >> 8 ) |
+                    (((val) & 0x00000000ff000000ull) << 8 ) |
+                    (((val) & 0x0000000000ff0000ull) << 24) |
+                    (((val) & 0x000000000000ff00ull) << 40) |
+                    (((val) & 0x00000000000000ffull) << 56));
+}
+
 }
