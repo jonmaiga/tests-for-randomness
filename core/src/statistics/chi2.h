7985300b08176bb2f65029b3bf32c1be1837e01f
jonmaiga <jon@jonkagstrom.com>
2022-11-12 14:01:32 +0100

    - More chi2 cleanup

--- core/src/statistics/chi2.h
+++ core/src/statistics/chi2.h
@@ -4,43 +4,44 @@
 
 #include "distributions.h"
 #include "streams.h"
+#include "util/algo.h"
 #include "util/math.h"
 
 namespace mixer {
+
+inline std::vector<uint64_t> bin_data_for_chi2(const std::vector<double>& data01) {
+	std::vector<uint64_t> bins(static_cast<uint64_t>(std::ceil(2 * pow(data01.size(), .4))));
+	const auto bin_count = static_cast<double>(bins.size());
+	for (const auto v : data01) {
+		assertion(is_valid_between_01(v), "Invalid data in chi2");
+		auto index = static_cast<std::size_t>(bin_count * v);
+		bins[std::min(bins.size() - 1, index)]++;
+	}
+	return bins;
+}
 
 struct chi2_statistics {
 	double chi2{};
 	double df{};
 };
 
-inline chi2_statistics compute_chi2_from_bins(const std::vector<uint64_t>& bins, double expected_count) {
+inline chi2_statistics chi2_from_bins(const std::vector<uint64_t>& bins, double expected_count) {
 	double chi2 = 0;
-	for (const auto bin : bins) {
-		const double diff = static_cast<double>(bin) - expected_count;
+	for (const auto count : bins) {
+		const double diff = static_cast<double>(count) - expected_count;
 		chi2 += diff * diff / expected_count;
 	}
 	return {chi2, static_cast<double>(bins.size() - 1)};
 }
 
-inline chi2_statistics chi2_stats(const std::vector<double>& data) {
-	const auto bin_count = static_cast<uint64_t>(std::ceil(2 * pow(data.size(), .4)));
-	std::vector<uint64_t> bins(bin_count);
-	const auto binCount = static_cast<double>(bins.size());
-	for (const auto v : data) {
-		auto index = static_cast<std::size_t>(binCount * v);
-		bins[std::min(bins.size() - 1, index)]++;
-	}
-	const double expected_count = static_cast<double>(data.size()) / binCount;
-	return compute_chi2_from_bins(bins, expected_count);
+inline chi2_statistics chi2_uniform_stats(const std::vector<double>& data01) {
+	const auto& bins = bin_data_for_chi2(data01);
+	const double expected_count = static_cast<double>(data01.size()) / static_cast<double>(bins.size());
+	return chi2_from_bins(bins, expected_count);
 }
 
 inline std::vector<statistic> chi2_test(const uint64_t n, const stream& stream) {
-	std::vector<double> data;
-	data.reserve(n);
-	for (uint64_t i = 0; i < n; ++i) {
-		data.push_back(rescale64_to_01(stream()));
-	}
-	const auto stat = chi2_stats(data);
+	const auto stat = chi2_uniform_stats(rescale64_to_01(n, stream));
 	return {{s_type::chi2, stat.chi2, chi2_distribution_cdf(stat.chi2, stat.df)}};
 }
 
