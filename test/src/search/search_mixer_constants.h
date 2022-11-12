9f4c427225f972d08e028c6a025731011cd1a74a
jonmaiga <jon@jonkagstrom.com>
2022-11-12 16:12:15 +0100

    - some search mixer rearrangements

--- test/src/search/search_mixer_constants.h
+++ test/src/search/search_mixer_constants.h
@@ -8,114 +8,55 @@
 
 namespace mixer {
 
-static thread_local random rnd;
-
 inline double sffs_fitness_test(const mixer& mixer) {
-	const auto r = test_rrc(mixer, 500);
-	//  std::vector<double> pvs;
-	//  for (const auto& tr : r.results) {
-	//  	append(pvs, to_p_values(tr.second));
-	// }
-	// auto bs = basic_stats(pvs);
-	// return count_fails(pvs, 0.005);
-
+	const auto r = test_rrc(mixer, 5000);
 	std::vector<double> all;
 	for (const auto& tr : r.results) {
-		all.push_back(fishers_combined_probabilities(rescale_to_01(to_p_values(tr.second))));
+		append(all, to_p_values(tr.second));
 	}
+	return kolmogorov_smirnov_stats(all);
 	const double pv = fishers_combined_probabilities(all);
 	return 2 * std::abs(pv - 0.5);
 }
 
-inline config get_mx3_config() {
-	struct constants {
-		explicit constants(const bit_vector& bits) {
-			//m1 = bits.get(0, 64);
-			C1 = bits.get(0, 6);
-			C2 = bits.get(6, 6);
-			C3 = bits.get(12, 6);
-			C4 = bits.get(18, 6);
-			m1 = bits.get(24, 64);
-			m2 = bits.get(24 + 64, 64);
-			m3 = bits.get(24 + 64 + 64, 64);
-		}
-
-		uint64_t C1 = 32;
-		uint64_t C2 = 29;
-		uint64_t C3 = 32;
-		uint64_t C4 = 29;
-		uint64_t m1;
-		uint64_t m2;
-		uint64_t m3;
-	};
-
-	auto factory = [](const bit_vector& bits) {
-		return [c=constants(bits)](uint64_t x) {
-			const uint64_t C = c.m1; //0xbea225f9eb34556d;
-			x ^= (x >> c.C1);
-			x *= c.m1;
-			x ^= (x >> c.C2);
-			x *= c.m2;
-			x ^= (x >> c.C3);
-			x *= c.m3;
-			x ^= (x >> c.C4);
-			return x;
-		};
-	};
-
-	auto to_str = [](const bit_vector& bits) {
-
-		const constants c(bits);
-		const uint64_t C = c.m1; //0xbea225f9eb34556d;
-		std::stringstream ss;
-		ss << "    x ^= x >> " << c.C1 << ";\n";
-		ss << "    x *= " << c.m1 << "ull;\n";
-		ss << "    x ^= x >> " << c.C2 << ";\n";
-		ss << "    x *= " << c.m2 << "ull;\n";
-		ss << "    x ^= x >> " << c.C3 << ";\n";
-		ss << "    x *= " << c.m3 << "ull;\n";
-		ss << "    x ^= x >> " << c.C4 << ";\n";
-		return ss.str();
-	};
-
-	auto to_arr_str = [](const bit_vector& bits) {
-		const constants c(bits);
-		std::stringstream ss;
-		ss << c.C1 << ", " << c.C2 << ", " << c.C3 << ", " << c.C4;
-		return ss.str();
-	};
-
-	constexpr int bits = 24 + 64 + 64 + 64; // + 64; // + 64 + 64 + 64;
-	const auto fitness = [factory](const bit_vector& bits) {
-		const auto m = mixer{"mx3", factory(bits)};
-		return sffs_fitness_test(m);
-	};
-	auto seed = bit_vector(bits);
-	seed.set(32, 0, 6);
-	seed.set(29, 6, 6);
-	seed.set(32, 12, 6);
-	seed.set(29, 18, 6);
-	seed.set(0xbea225f9eb34556d, 24, 64);
-	seed.set(0xbea225f9eb34556d, 24 + 64, 64);
-	seed.set(0xbea225f9eb34556d, 24 + 64 + 64, 64);
-	return {bits, seed, fitness, to_str, to_arr_str};
+inline bit_vector random_bit_vector(int bits) {
+	static random r(1234);
+	bit_vector bv(bits);
+	for (int b = 0; b < bits; ++b) {
+		bv.set_bit(b, r() & 1);
+	}
+	return bv;
 }
 
-inline void start_tune(const std::string& name, const config& config) {
+inline bit_vector find_seed(const config& config) {
+	double best_score = std::numeric_limits<double>::max();
+	bit_vector best(config.bits);
+	for (int i = 0; i < 30; ++i) {
+		bit_vector r = random_bit_vector(config.bits);
+		const double score = config.fitness(r);
+		if (score < best_score) {
+			best_score = score;
+			best = r;
+		}
+	}
+	return best;
+}
+
+inline void start_search(const std::string& name, const config& config) {
+	const auto trng_stream = create_stream_from_data_by_ref_thread_safe("trng", get_trng_data());
+	const auto trng = create_mixer_from_stream("trng1", trng_stream);
+
 	std::cout << "===========================\n";
 	std::cout << name << " " << config.bits << " bits\n";
 	std::cout << "===========================\n";
 	std::cout << config.to_string(config.seed) << "\n";
 	std::cout << "Baseline fitness: " << config.fitness(config.seed) << "\n";
+	std::cout << "Trng fitness    : " << sffs_fitness_test(trng) << "\n";
 	const auto result = run_sffs(config, create_sffs_printer(config.to_arr_str));
 	std::stringstream ss;
 	ss << to_string(result, config.to_arr_str) << "\n";
 	ss << config.to_string(result.data);
 	std::cout << ss.str();
-}
-
-inline void run_search() {
-	start_tune("mx3", get_mx3_config());
 }
 
 }
---
+++ test/src/search/search_mixer_constants_setup.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include "search_mixer_constants.h"
+
+namespace mixer {
+
+inline config get_mx3_config() {
+	struct constants {
+		explicit constants(const bit_vector& bits) {
+			//m1 = bits.get(0, 64);
+			C1 = bits.get(0, 6);
+			C2 = bits.get(6, 6);
+			C3 = bits.get(12, 6);
+			C4 = bits.get(18, 6);
+			//m1 = bits.get(24, 64);
+			//m2 = bits.get(24 + 64, 64);
+			//m3 = bits.get(24 + 64 + 64, 64);
+		}
+
+		uint64_t C1 = 32;
+		uint64_t C2 = 29;
+		uint64_t C3 = 32;
+		uint64_t C4 = 29;
+		//uint64_t m1;
+		//uint64_t m2;
+		//uint64_t m3;
+	};
+
+	auto factory = [](const bit_vector& bits) {
+		return [c=constants(bits)](uint64_t x) {
+			const uint64_t C = 0xbea225f9eb34556d;
+			x ^= (x >> c.C1);
+			x *= C;
+			x ^= (x >> c.C2);
+			x *= C;
+			x ^= (x >> c.C3);
+			x *= C;
+			x ^= (x >> c.C4);
+			return x;
+		};
+	};
+
+	auto to_str = [](const bit_vector& bits) {
+
+		const constants c(bits);
+		const uint64_t C = 0xbea225f9eb34556d;
+		std::stringstream ss;
+		ss << "    x ^= x >> " << c.C1 << ";\n";
+		ss << "    x *= " << C << "ull;\n";
+		ss << "    x ^= x >> " << c.C2 << ";\n";
+		ss << "    x *= " << C << "ull;\n";
+		ss << "    x ^= x >> " << c.C3 << ";\n";
+		ss << "    x *= " << C << "ull;\n";
+		ss << "    x ^= x >> " << c.C4 << ";\n";
+		return ss.str();
+	};
+
+	auto to_arr_str = [](const bit_vector& bits) {
+		const constants c(bits);
+		std::stringstream ss;
+		ss << c.C1 << ", " << c.C2 << ", " << c.C3 << ", " << c.C4;
+		return ss.str();
+	};
+
+	constexpr int bits = 24; // + 64; // + 64 + 64; // + 64; // + 64 + 64 + 64;
+	const auto fitness = [factory](const bit_vector& bits) {
+		const auto m = mixer{"mx3", factory(bits)};
+		return sffs_fitness_test(m);
+	};
+	auto seed = bit_vector(bits);
+	seed.set(32, 0, 6);
+	seed.set(29, 6, 6);
+	seed.set(32, 12, 6);
+	seed.set(29, 18, 6);
+	//seed.set(0xbea225f9eb34556d, 24, 64);
+	//seed.set(0xbea225f9eb34556d, 24 + 64, 64);
+	//seed.set(0xbea225f9eb34556d, 24 + 64 + 64, 64);
+	return {bits, seed, fitness, to_str, to_arr_str};
+}
+
+inline config get_xmx_config() {
+	struct constants {
+		explicit constants(const bit_vector& bits) {
+			//m1 = bits.get(0, 64);
+			C1 = bits.get(0, 6);
+			C2 = bits.get(6, 6);
+			m1 = bits.get(12, 64);
+		}
+
+		uint64_t C1 = 32;
+		uint64_t C2 = 29;
+		uint64_t m1;
+	};
+
+	auto factory = [](const bit_vector& bits) {
+		return [c=constants(bits)](uint64_t x) {
+			const uint64_t C = 0xbea225f9eb34556d;
+			x ^= (x >> c.C1);
+			x *= c.m1;
+			x ^= (x >> c.C2);
+			return x;
+		};
+	};
+
+	auto to_str = [](const bit_vector& bits) {
+
+		const constants c(bits);
+		const uint64_t C = 0xbea225f9eb34556d;
+		std::stringstream ss;
+		ss << "    x ^= x >> " << c.C1 << ";\n";
+		ss << "    x *= " << c.m1 << "ull;\n";
+		ss << "    x ^= x >> " << c.C2 << ";\n";
+		return ss.str();
+	};
+
+	auto to_arr_str = [](const bit_vector& bits) {
+		const constants c(bits);
+		std::stringstream ss;
+		ss << c.C1 << ", " << c.C2 << ", " << c.m1;
+		return ss.str();
+	};
+
+	constexpr int bits = 12 + 64;
+	const auto fitness = [factory](const bit_vector& bits) {
+		const auto m = mixer{"xmx", factory(bits)};
+		return sffs_fitness_test(m);
+	};
+	auto seed = bit_vector(bits);
+	seed.set(5, 0, 6);
+	seed.set(1, 6, 6);
+	seed.set(2534203720592697647ull, 12, 64);
+	return {bits, seed, fitness, to_str, to_arr_str};
+}
+
+inline void run_search() {
+	auto cfg = get_xmx_config();
+	cfg.seed = find_seed(cfg);
+	start_search("xmx", cfg);
+}
+
+}
