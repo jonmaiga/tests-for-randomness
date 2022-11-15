84109e8c4c568b67ed669f056fd9729b454ce6fc
jonmaiga <jon@jonkagstrom.com>
2022-11-15 12:10:40 +0100

    - calculate max gap size

--- core/src/statistics/gap.h
+++ core/src/statistics/gap.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cmath>
-#include <numeric>
 #include <vector>
 
 #include "chi2.h"
@@ -12,11 +11,11 @@
 namespace mixer {
 
 inline std::vector<uint64_t> generate_gaps(uint64_t max_gap_size, double a, double b, const std::vector<double>& data01) {
-	std::vector<uint64_t> gaps(max_gap_size + 1);
+	std::vector<uint64_t> gaps(max_gap_size);
 	uint64_t current_gap = 0;
 	for (const auto v : data01) {
 		if (v >= a && v < b) {
-			gaps[std::min(current_gap, max_gap_size)] ++;
+			gaps[std::min(current_gap, gaps.size() - 1)] ++;
 			current_gap = 0;
 		}
 		else {
@@ -26,31 +25,30 @@ inline std::vector<uint64_t> generate_gaps(uint64_t max_gap_size, double a, doub
 	return gaps;
 }
 
-inline std::vector<double> generate_gap_probabilities(uint64_t max_gap_size, double a, double b) {
-	std::vector<double> ps(max_gap_size + 1);
+inline std::vector<double> generate_gap_probabilities(double a, double b) {
+	std::vector<double> ps;
 	double p_sum = 0;
-	for (std::size_t i = 0; i < max_gap_size; ++i) {
-		ps[i] = (b - a) * std::pow(1. - b + a, i);
-		p_sum += ps[i];
+	std::size_t i = 0;
+	while (p_sum < 0.99) {
+		ps.push_back((b - a) * std::pow(1. - b + a, i));
+		p_sum += ps.back();
+		++i;
 	}
-	ps[max_gap_size] = 1. - p_sum;
+	ps.push_back(1. - p_sum);
 	assertion(is_valid_between_01(ps.back()), "Weird gap probabilities");
 	return ps;
 }
 
 
 inline chi2_statistics gap_stats(const std::vector<double>& data01) {
-	constexpr uint64_t max_gap_size = 7;
 	double a = 0.35;
 	double b = 0.65;
-	const auto& ps = generate_gap_probabilities(max_gap_size, a, b);
-	const auto& gaps = generate_gaps(max_gap_size, a, b, data01);
+	const auto& ps = generate_gap_probabilities(a, b);
+	const auto& gaps = generate_gaps(ps.size(), a, b, data01);
 	const auto total_count = accumulate(gaps);
-	return chi2_stats(
-		gaps.size(),
-		to_data(gaps),
-		mul(to_data(ps), to_data(total_count)),
-		5.);
+	return chi2_stats(gaps.size(), to_data(gaps),
+	                  mul(to_data(ps), to_data(total_count)),
+	                  5.);
 }
 
 inline std::vector<statistic> gap_test(uint64_t n, const stream& stream) {
--- unittest/src/statistics/gap_test.cpp
+++ unittest/src/statistics/gap_test.cpp
@@ -7,29 +7,29 @@
 namespace mixer {
 
 TEST(gap, generate_gaps) {
-	EXPECT_EQ(generate_gaps(2, 0, 1, {}), (std::vector<uint64_t>{0, 0, 0}));
-	EXPECT_EQ(generate_gaps(2, 0, 1, {0.5}), (std::vector<uint64_t>{1, 0, 0}));
-	EXPECT_EQ(generate_gaps(2, 0, 1, {0.25, 0.75}), (std::vector<uint64_t>{2, 0, 0}));
+	EXPECT_EQ(generate_gaps(2, 0, 1, {}), (std::vector<uint64_t>{0, 0}));
+	EXPECT_EQ(generate_gaps(2, 0, 1, {0.5}), (std::vector<uint64_t>{1, 0}));
+	EXPECT_EQ(generate_gaps(2, 0, 1, {0.25, 0.75}), (std::vector<uint64_t>{2, 0}));
 
-	EXPECT_EQ(generate_gaps(2, 0, .5, {0.25, 0.75}), (std::vector<uint64_t>{1, 0, 0}));
-	EXPECT_EQ(generate_gaps(2, 0, .5, {0.75, 0.25}), (std::vector<uint64_t>{0, 1, 0}));
-	EXPECT_EQ(generate_gaps(2, 0, .5, {0.75, 0.25, 0.5, 0.25}), (std::vector<uint64_t>{0, 2, 0}));
-	EXPECT_EQ(generate_gaps(2, 0, .5, {0.25, 0.75, 0.25, 0.5, 0.25}), (std::vector<uint64_t>{1, 2, 0}));
-	EXPECT_EQ(generate_gaps(2, .5, .6, {0.25, 0.5}), (std::vector<uint64_t>{0, 1, 0}));
+	EXPECT_EQ(generate_gaps(2, 0, .5, {0.25, 0.75}), (std::vector<uint64_t>{1, 0}));
+	EXPECT_EQ(generate_gaps(2, 0, .5, {0.75, 0.25}), (std::vector<uint64_t>{0, 1}));
+	EXPECT_EQ(generate_gaps(2, 0, .5, {0.75, 0.25, 0.5, 0.25}), (std::vector<uint64_t>{0, 2}));
+	EXPECT_EQ(generate_gaps(2, 0, .5, {0.25, 0.75, 0.25, 0.5, 0.25}), (std::vector<uint64_t>{1, 2}));
+	EXPECT_EQ(generate_gaps(2, .5, .6, {0.25, 0.5}), (std::vector<uint64_t>{0, 1}));
 }
 
 TEST(gap, generate_too_large_gaps) {
-	EXPECT_EQ(generate_gaps(2, .5, .6, {0.25, 0.35, 0.52, 0.25, 0.35, 0.52}), (std::vector<uint64_t>{0, 0, 2}));
+	EXPECT_EQ(generate_gaps(2, .5, .6, {0.25, 0.35, 0.52, 0.25, 0.35, 0.52}), (std::vector<uint64_t>{0, 2}));
 }
 
 TEST(gap, generate_gap_probabilities) {
-	const auto ps = generate_gap_probabilities(5, .1, .2);
+	const auto ps = generate_gap_probabilities(.1, .2);
 	EXPECT_NEAR(ps[0], 0.1, 1e-4);
 	EXPECT_NEAR(ps[1], 0.09, 1e-4);
 	EXPECT_NEAR(ps[2], 0.081, 1e-4);
 	EXPECT_NEAR(ps[3], 0.0729, 1e-4);
 	EXPECT_NEAR(ps[4], 0.06561, 1e-4);
-	EXPECT_NEAR(ps[5], 0.59049, 1e-4);
+	EXPECT_NEAR(ps[5], 0.0590, 1e-4);
 }
 
 TEST(gap, no_change) {
