16e82482e2376439633feb4ffaee99c3a6d92a76
jonmaiga <jon@jonkagstrom.com>
2022-11-17 06:34:07 +0100

    - Fixed test

--- unittest/src/test/permutation_test.cpp
+++ unittest/src/test/permutation_test.cpp
@@ -10,8 +10,8 @@ namespace mixer {
 TEST(permutation, no_change) {
 	const auto r = permutation_test(1000, test_stream());
 	EXPECT_EQ(r.size(), 1);
-	EXPECT_NEAR(r.front().value, 7.4625, 1e-4);
-	EXPECT_NEAR(r.front().p_value, 0.7853, 1e-4);
+	EXPECT_NEAR(r.front().value, 36.3470, 1e-4);
+	EXPECT_NEAR(r.front().p_value, 0.2335, 1e-4);
 }
 
 }
