66f5289c5bf9b37929801b8318ca0192b8797418
jonmaiga <jon@jonkagstrom.com>
2022-11-21 13:48:38 +0100

    - fixed rrc for 32 bit

--- core/src/rrc.h
+++ core/src/rrc.h
@@ -14,21 +14,13 @@ enum class rrc_type {
 
 const auto rrc_types = {rrc_type::identity, rrc_type::reverse, rrc_type::complement, rrc_type::reverse_complement};
 
-inline uint64_t reverse64(uint64_t x) {
-	x = (x & 0xaaaaaaaaaaaaaaaaull) >> 1 | (x & 0x5555555555555555ull) << 1;
-	x = (x & 0xccccccccccccccccull) >> 2 | (x & 0x3333333333333333ull) << 2;
-	x = (x & 0xf0f0f0f0f0f0f0f0ull) >> 4 | (x & 0x0f0f0f0f0f0f0f0full) << 4;
-	x = (x & 0xff00ff00ff00ff00ull) >> 8 | (x & 0x00ff00ff00ff00ffull) << 8;
-	x = (x & 0xffff0000ffff0000ull) >> 16 | (x & 0x0000ffff0000ffffull) << 16;
-	return x >> 32 | x << 32;
-}
-
-inline uint64_t permute(uint64_t c, int rot, rrc_type type) {
+template <typename T>
+uint64_t permute(T c, int rot, rrc_type type) {
 	switch (type) {
 	case rrc_type::identity: return ror(c, rot);
-	case rrc_type::reverse: return ror(reverse64(c), rot);
+	case rrc_type::reverse: return ror(reverse_bits(c), rot);
 	case rrc_type::complement: return ror(~c, rot);
-	case rrc_type::reverse_complement: return ror(~reverse64(c), rot);
+	case rrc_type::reverse_complement: return ror(~reverse_bits(c), rot);
 	}
 	assertion(false, "unknown type");
 	return 0;
