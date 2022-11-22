3bedd88d331fe565bf9b4af81e16ae6ee4679ca5
jonmaiga <jon@jonkagstrom.com>
2022-11-22 14:19:08 +0100

    - sliding windows works with ranged - permutation for large n

--- unittest/src/statistics/waldwolfowitz_test.cpp
+++ unittest/src/statistics/waldwolfowitz_test.cpp
@@ -7,42 +7,42 @@
 namespace mixer {
 
 TEST(waldwolfowitz, empty) {
-	const auto ww = wald_wolfowitz_stats<uint64_t>({});
+	const auto ww = generate_wald_wolfowitz_data<uint64_t>({});
 	EXPECT_EQ(ww.runs, 0);
 	EXPECT_EQ(ww.n_plus, 0);
 	EXPECT_EQ(ww.n_minus, 0);
 }
 
 TEST(waldwolfowitz, one_element) {
-	const auto ww = wald_wolfowitz_stats<uint64_t>({1});
+	const auto ww = generate_wald_wolfowitz_data<uint64_t>({1});
 	EXPECT_EQ(ww.runs, 1);
 	EXPECT_EQ(ww.n_plus, 0);
 	EXPECT_EQ(ww.n_minus, 0);
 }
 
 TEST(waldwolfowitz, two_same) {
-	const auto ww = wald_wolfowitz_stats<uint64_t>({1, 1});
+	const auto ww = generate_wald_wolfowitz_data<uint64_t>({1, 1});
 	EXPECT_EQ(ww.runs, 1);
 	EXPECT_EQ(ww.n_plus, 0);
 	EXPECT_EQ(ww.n_minus, 0);
 }
 
 TEST(waldwolfowitz, two_different) {
-	const auto ww = wald_wolfowitz_stats<double>({1, 2});
+	const auto ww = generate_wald_wolfowitz_data<double>({1, 2});
 	EXPECT_EQ(ww.runs, 2);
 	EXPECT_EQ(ww.n_plus, 1);
 	EXPECT_EQ(ww.n_minus, 1);
 }
 
 TEST(waldwolfowitz, two_runs) {
-	const auto ww = wald_wolfowitz_stats<uint64_t>({1, 1, 1, 2, 2, 2});
+	const auto ww = generate_wald_wolfowitz_data<uint64_t>({1, 1, 1, 2, 2, 2});
 	EXPECT_EQ(ww.runs, 2);
 	EXPECT_EQ(ww.n_plus, 3);
 	EXPECT_EQ(ww.n_minus, 3);
 }
 
 TEST(waldwolfowitz, two_runs_2) {
-	const auto ww = wald_wolfowitz_stats<double>({3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1});
+	const auto ww = generate_wald_wolfowitz_data<double>({3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1});
 	EXPECT_EQ(ww.runs, 2);
 	EXPECT_EQ(ww.n_plus, 3);
 	EXPECT_EQ(ww.n_minus, 5);
