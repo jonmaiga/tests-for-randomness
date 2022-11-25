09f2925e9286d19d3f18f1369e80958b89b9700c
jonmaiga <jon@jonkagstrom.com>
2022-11-25 08:25:39 +0100

    - added avalanche sac with 200k data points test

--- unittest/src/statistics/avalanche_test.cpp
+++ unittest/src/statistics/avalanche_test.cpp
@@ -15,6 +15,15 @@ TEST(avalanche, bic_data_no_change) {
 	EXPECT_EQ(hash64(avalanche_generate_bic(10, test_stream(), mx3)), 3526828816776552911ull);
 }
 
+TEST(avalanche, sac_data_large) {
+	using T = uint32_t;
+	constexpr auto n = 200000;
+	const auto counts = avalanche_generate_sac<T>(n, test_stream<T>(), mix32::mx3);
+	auto s = avalanche_sac_stats<T>(n, counts);
+	EXPECT_NEAR(s->value, 17.7571, 1e-4);
+	EXPECT_NEAR(s->p_value, 0.8844, 1e-4);
+}
+
 TEST(avalanche, sac_no_change) {
 	const auto r = avalanche_mixer_sac_test(50, test_stream(), mx3).front().stats;
 	EXPECT_NEAR(r->value,  17.1819, 1e-4);
