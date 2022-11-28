9d29642aace6122469687669c256b26c928bf62e
jonmaiga <jon@jonkagstrom.com>
2022-11-28 14:51:52 +0100

    - refactoring

--- core/src/eval_parallel.h
+++ core/src/eval_parallel.h
@@ -39,31 +39,35 @@ test_jobs create_test_jobs(
 	uint64_t n,
 	const mixer<T>& mix,
 	const test_definition<T>& test_def,
-	const std::vector<source<T>>& sources) {
+	const source<T>& source) {
 	test_jobs js;
-	for (const auto& source : sources) {
+
+	if (test_def.test_mixer && !source.stream_append_factory) {
 		js.push_back([test_def, source, mix, n]()-> test_job_return {
 			std::vector<test_result> results;
-			if (const auto& mixer_test = test_def.test_mixer) {
-				if (!source.stream_append_factory) {
-					for (const auto& sub_test : mixer_test(n, source.stream_source, mix)) {
-						if (const auto& stat = sub_test.stats) {
-							results.push_back({source.stream_source.name, mix.name, n, {test_def.type, sub_test.name}, *stat});
-						}
-					}
-				}
-			}
-			if (const auto& stream_test = test_def.test_stream) {
-				const auto s = create_stream(mix, source);
-				for (const auto& sub_test : stream_test(n, s)) {
-					if (const auto& stat = sub_test.stats) {
-						results.push_back(test_result{s.name, mix.name, n, {test_def.type, sub_test.name}, *stat});
-					}
+			for (const auto& sub_test : test_def.test_mixer(n, source.stream_source, mix)) {
+				if (const auto& stat = sub_test.stats) {
+					results.push_back({source.stream_source.name, mix.name, n, {test_def.type, sub_test.name}, *stat});
 				}
 			}
 			return results;
 		});
 	}
+
+	if (test_def.test_stream) {
+		const auto s = create_stream(mix, source);
+		js.push_back([test_def, source = s, mix, n]()-> test_job_return {
+			std::vector<test_result> results;
+			for (const auto& sub_test : test_def.test_stream(n, source)) {
+				if (const auto& stat = sub_test.stats) {
+					results.push_back(test_result{source.name, mix.name, n, {test_def.type, sub_test.name}, *stat});
+				}
+			}
+			return results;
+		});
+
+	}
+
 	return js;
 }
 
@@ -71,7 +75,9 @@ template <typename T>
 test_jobs create_test_jobs(const test_setup<T>& setup) {
 	test_jobs jobs;
 	for (const auto& test : setup.tests) {
-		append(jobs, create_test_jobs<T>(setup.n, setup.mix, get_test_definition<T>(test), setup.sources));
+		for (const auto& source : setup.sources) {
+			append(jobs, create_test_jobs<T>(setup.n, setup.mix, get_test_definition<T>(test), source));
+		}
 	}
 	return jobs;
 }
