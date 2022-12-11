6a46d46110fb06e4195e4bbd07995c5e6b11796c
jonmaiga <jon@jonkagstrom.com>
2022-12-11 12:04:30 +0100

    - added trng to ppm

--- test/src/command/ppm_command.h
+++ test/src/command/ppm_command.h
@@ -6,8 +6,15 @@ namespace mixer {
 
 template <typename T>
 void ppm_command() {
+	constexpr int size = 512;
+	auto trng_stream = create_stream_from_data_by_ref<T>("trng", get_trng_data<T>());
+	write_ppm<T>(
+		get_config().root_path + "ppm/" + trng_stream.name + ".ppm",
+		size, size,
+		[trng_stream](T, T) mutable { return trng_stream(); });
+
 	for (const auto& mix : get_mixers<T>()) {
-		constexpr int size = 256;
+
 		write_ppm<T>(
 			get_config().root_path + "ppm/" + mix.name + ".ppm",
 			size, size,
