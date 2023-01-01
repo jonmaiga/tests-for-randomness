ba2f589064c7da238086266b1a6fb59b272c1250
jonkagstrom <jon@jonkagstrom.com>
2023-01-01 17:33:42 +0100

    - clean up

--- test/src/util/stream_sources.h
+++ test/src/util/stream_sources.h
@@ -24,7 +24,6 @@ streams<T> create_sources() {
 
 template <typename T>
 streams<T> create_combiner_sources(combiner<T> combiner) {
-	const auto& mix = get_default_mixer<T>();
 	streams<T> sources;
 	// 0, 0
 	sources.push_back(create_combined_incremental_stream<T>(
@@ -36,32 +35,32 @@ streams<T> create_combiner_sources(combiner<T> combiner) {
 		create_counter_stream<T>(1, 0),
 		combiner));
 
-	// a, 0 - should this be tested? 
+	// a, 0
 	sources.push_back(create_combined_stream<T>(
 		create_counter_stream<T>(1, 0),
 		create_constant_stream<T>(0),
 		combiner));
 
+	const auto& mix = get_default_mixer<T>();
 	streams<T> streams_a;
 	streams<T> streams_b;
 
-	// 17 a, b
+	// a, b
 	constexpr int seed = 0;
 	streams_a.push_back(create_counter_stream<T>(seed + 1, mix(1000 + seed)));
 	streams_b.push_back(create_counter_stream<T>(seed + 1, mix(1 + seed)));
 
-	// 17, >23 a, a
+	// a, a
 	streams_a.push_back(create_counter_stream<T>(1, mix(seed)));
 	streams_b.push_back(create_counter_stream<T>(1, mix(seed)));
 
-	// 17 a, -a
+	// a, -a
 	streams_a.push_back(create_counter_stream<T>(1, mix(seed)));
 	streams_b.push_back(create_counter_stream<T>(1, -mix(seed)));
 
 	const auto rrc_a = create_rrc_sources(streams_a);
 	const auto rrc_b = create_rrc_sources(streams_b);
 	for (std::size_t i = 0; i < rrc_a.size(); ++i) {
-		// 16, >22
 		sources.push_back(create_combined_stream(rrc_a[i], rrc_b[i], combiner));
 	}
 
