3b3b119a07ab46dd9cca7371cad6e1759ef60d7c
jonmaiga <jon@jonkagstrom.com>
2023-01-06 09:31:44 +0100

    - prng cleanup

--- impl/src/prngs32.h
+++ impl/src/prngs32.h
@@ -10,33 +10,32 @@ namespace mixer {
     
 namespace prng32 {
 
-stream<uint32_t> xmx(uint32_t seed) {
+inline stream<uint32_t> xmx(uint32_t seed) {
     return {"xmx", [state = seed]() mutable {
         return state = mix32::xmx(state);
     }};
 }
 
-stream<uint32_t> xm2x(uint32_t seed) {
+inline stream<uint32_t> xm2x(uint32_t seed) {
     return {"xm2x", [state = seed]() mutable {
         return state = mix32::xm2x(state);
     }};
 }
 
-stream<uint32_t> xm3x(uint32_t seed) {
+inline stream<uint32_t> xm3x(uint32_t seed) {
     return {"xm3x", [state = seed]() mutable {
         return state = mix32::xm3x(state);
     }};
 }
 
-
-stream<uint32_t> pcg(uint32_t seed) {
+inline stream<uint32_t> pcg(uint32_t seed) {
     pcg32 pcg(seed);
     return {"pcg", [pcg]() mutable {
-        return static_cast<uint32_t>(pcg());
+        return pcg();
     }};
 }
 
-stream<uint32_t> xoroshift(uint32_t seed) {
+inline stream<uint32_t> xoroshift(uint32_t seed) {
     return {"xoroshift", [x = seed]() mutable {
         /* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
         x ^= x << 13;
@@ -46,14 +45,14 @@ stream<uint32_t> xoroshift(uint32_t seed) {
     }};
 }
 
-stream<uint32_t> mt(uint32_t seed) {
+inline stream<uint32_t> mt19337(uint32_t seed) {
     std::mt19937 gen(seed);
     return {"mt", [gen]() mutable {
         return gen();
     }};
 }
 
-stream<uint32_t> minstd_rand(uint32_t seed) {
+inline stream<uint32_t> minstd_rand(uint32_t seed) {
     std::minstd_rand gen(seed);
     return {"minstd_rand", [gen]() mutable {
         return gen();
@@ -67,7 +66,7 @@ template <typename T> std::vector<stream<T>> get_prngs(T seed) = delete;
 template <>
 inline std::vector<stream<uint32_t>> get_prngs(uint32_t seed) {
 	return {
-        prng32::mt(seed), // > 31
+        prng32::mt19337(seed), // > 31
         prng32::xmx(seed),  // 29 uniform-m     failure (10) xm3x(p=1.000000)  
         prng32::xm2x(seed), // 29 uniform-m     failure (10) xm2x(p=0.000000) 
         prng32::xm3x(seed), // 29 uniform-m     failure (10) xm3x(p=1.000000)  
--- impl/src/prngs64.h
+++ impl/src/prngs64.h
@@ -7,34 +7,34 @@ namespace mixer {
 
 namespace prng64 {
 
-stream<uint64_t> xmx(uint64_t seed) {
+inline stream<uint64_t> xmx(uint64_t seed) {
     return {"xmx", [state = seed]() mutable {
         return state = mix64::xmx(state);
     }};
 }
 
-stream<uint64_t> xm2x(uint64_t seed) {
+inline stream<uint64_t> xm2x(uint64_t seed) {
     return {"xm2x", [state = seed]() mutable {
         return state = mix64::xm2x(state);
     }};
 }
 
-stream<uint64_t> xm3x(uint64_t seed) {
+inline stream<uint64_t> xm3x(uint64_t seed) {
     return {"xm3x", [state = seed]() mutable {
         return state = mix64::mx3(state);
     }};
 }
 
-stream<uint64_t> pcg(uint64_t seed) {
+inline stream<uint64_t> pcg(uint64_t seed) {
     pcg64 pcg(seed);
     return {"pcg", [pcg]() mutable {
         return pcg();
     }};
 }
 
-stream<uint64_t> xoroshiftplus(uint64_t seed) {
+inline stream<uint64_t> xoroshiroplus(uint64_t seed) {
     uint64_t s[]{seed, seed};
-    return {"xoroshiftplus", [s]() mutable {
+    return {"xoroshiroplus", [s]() mutable {
         const uint64_t s0 = s[0];
         uint64_t s1 = s[1];
         const uint64_t result = s0 + s1;
@@ -52,7 +52,7 @@ template<>
 inline std::vector<stream<uint64_t>> get_prngs(uint64_t seed) {
 	return {
         prng64::pcg(seed), // >31 suspicous (4)
-        //prng64::xoroshiftplus(seed), // >31
+        //prng64::xoroshiroplus(seed), // >31
         //prng64::xmx(seed), // >31
         //prng64::xm2x(seed), // >31
         //prng64::xm3x(seed), // >31
