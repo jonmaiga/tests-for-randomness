39dfc45b849848a5ca3681a7558be5815c678a1b
jonmaiga <jon@jonkagstrom.com>
2023-01-08 13:58:26 +0100

    - some cleanup

--- core/src/statistics/bit_correlation.h
+++ core/src/statistics/bit_correlation.h
@@ -8,21 +8,13 @@
 
 namespace mixer {
 
-// bit_count(x) -> binomial of bit_count y
 template <typename T>
 std::vector<std::vector<uint64_t>> bit_count_correlation_matrix(uint64_t n, stream<T> stream) {
-	constexpr auto Bits = bit_sizeof<T>();
-	constexpr auto Size = Bits + 1;
-
-	std::vector<std::vector<uint64_t>> counts(Size);
-	for (auto& v : counts) {
-		v.resize(Size);
-	}
+	constexpr auto Size = bit_sizeof<T>() + 1;
+	std::vector<std::vector<uint64_t>> counts(Size, std::vector<uint64_t>(Size));
 	for (uint64_t i = 0; i < n; ++i) {
-		const T x = stream();
-		const T y = stream();
-		const auto x_count = bit_count(x);
-		const auto y_count = bit_count(y);
+		const auto x_count = bit_count(stream());
+		const auto y_count = bit_count(stream());
 		counts[x_count][y_count] += 1;
 	}
 	return counts;
@@ -30,9 +22,7 @@ std::vector<std::vector<uint64_t>> bit_count_correlation_matrix(uint64_t n, stre
 
 template <typename T>
 std::optional<statistic> bit_count_correlation_matrix_stats(const uint64_t n, const std::vector<uint64_t>& bit_counts, int bit) {
-	constexpr auto Size = bit_sizeof<T>() + 1;
 	const double expected_total_count = n * binomial_pdf(bit_sizeof<T>(), .5, bit);
-
 	return chi2_stats(bit_counts.size(), to_data(bit_counts), [expected_total_count](std::size_t i) {
 		return expected_total_count * binomial_pdf(bit_sizeof<T>(), .5, i);
 	}, 5.);
@@ -44,7 +34,7 @@ sub_test_results bit_count_correlation_test(uint64_t n, const stream<T>& stream)
 	sub_test_results results;
 	for (int bit = 0; bit < bit_sizeof<T>(); ++bit) {
 		if (const auto stat = bit_count_correlation_matrix_stats<T>(n, counts[bit], bit)) {
-			results.push_back({"b-" + std::to_string(bit), stat});
+			results.push_back({"bit" + std::to_string(bit), stat});
 		}
 	}
 	return results;
