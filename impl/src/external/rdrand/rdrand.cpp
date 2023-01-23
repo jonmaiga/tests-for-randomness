7685581026599227556e27201b728ab9af1cf508
jonmaiga <jon@jonkagstrom.com>
2023-01-23 21:41:20 +0100

    - gcc fixes

--- impl/src/external/rdrand/rdrand.cpp
+++ impl/src/external/rdrand/rdrand.cpp
@@ -20,11 +20,12 @@ uint32_t rdrand_32() {
 }
 
 uint64_t rdrand_64() {
-	uint64_t x = 0;
+	static_assert(sizeof(unsigned long long) == sizeof(uint64_t));
+	unsigned long long x = 0;
 	for (int i = 0; i < 10; i++) {
-
-		if (_rdrand64_step(&x))
+		if (_rdrand64_step(&x)) {
 			return x;
+		}
 	}
 	assertion(false, "rdrand failed");
 	std::cout << "FAILURE";
