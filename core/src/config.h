3b0116c77ee8170e3dc0d72ff31129c3eaa24a30
jonmaiga <jon@jonkagstrom.com>
2023-01-24 13:39:37 +0100

    - added drng path

--- core/src/config.h
+++ core/src/config.h
@@ -9,16 +9,24 @@ struct config {
 	std::string root_path;
 
 	std::string trng_path() const {
-		return root_path + "data/trng.bin";
+		return data_path() + "trng.bin";
+	}
+
+	std::string drng_path() const {
+		return data_path() + "drng.bin";
 	}
 
 	std::string result_path() const {
 		return root_path;
 	}
 
-	template<typename T>
+	template <typename T>
 	std::string ppm_dir() const {
 		return root_path + "ppm" + std::to_string(bit_sizeof<T>()) + "/";
+	}
+
+	std::string data_path() const {
+		return root_path + "data/";
 	}
 };
 
