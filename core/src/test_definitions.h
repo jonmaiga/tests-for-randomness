11d308928007e8bd359eb7e5478d86e5857febba
jonmaiga <jon@jonkagstrom.com>
2023-01-31 12:59:40 +0100

    - removed old comments

--- core/src/test_definitions.h
+++ core/src/test_definitions.h
@@ -28,21 +28,21 @@ std::vector<test_definition<T>> get_tests() {
 		{test_type::mean, mean_test<T>, {}, "mean"},
 		{test_type::uniform, uniform_test<T>, {}, "uniform"},
 
-		{test_type::wald_wolfowitz_runs, wald_wolfowitz_test<T>, {}, "ww"}, // weak=xmx
-		// {test_type::pearson_r, pearson_correlation_test<T>, {}, "pearson_r"}, // weak
-		// {test_type::spearman_r, spearman_correlation_test<T>, {}, "spearman_r"}, // weak
-		// {test_type::kendall_tau, kendall_correlation_test<T>, {}, "kendall_tau"}, // slow todo
+		{test_type::wald_wolfowitz_runs, wald_wolfowitz_test<T>, {}, "ww"},
+		// {test_type::pearson_r, pearson_correlation_test<T>, {}, "pearson_r"},
+		// {test_type::spearman_r, spearman_correlation_test<T>, {}, "spearman_r"},
+		// {test_type::kendall_tau, kendall_correlation_test<T>, {}, "kendall_tau"},
 		{test_type::bit_count_2d, bit_count_2d_test<T>, {}, "bc2d"},
 		{test_type::bit_count_3d, bit_count_3d_test<T>, {}, "bc3d"},
 
-		{test_type::gap, gap_test<T>, {}, "gap"}, // weak (found no tweak)
-		{test_type::coupon, coupon_test<T>, {}, "coupon"}, // weak (found no tweak)
-		{test_type::divisibility, divisibility_test<T>, {}, "divisibility"}, // weak (didn't find any tweak)
-		{test_type::permutation, permutation_test<T>, {}, "permutation"}, // weak - but seems tweak-able (might be to little data but w4 also fails 64-bit-xm2x)
+		{test_type::gap, gap_test<T>, {}, "gap"},
+		{test_type::coupon, coupon_test<T>, {}, "coupon"},
+		{test_type::divisibility, divisibility_test<T>, {}, "divisibility"},
+		{test_type::permutation, permutation_test<T>, {}, "permutation"},
 
 		// mixer tests
-		{test_type::sac, {}, avalanche_mixer_sac_test<T>, "sac"}, // strong
-		{test_type::bic, {}, avalanche_mixer_bic_test<T>, "bic"}, // strong
+		{test_type::sac, {}, avalanche_mixer_sac_test<T>, "sac"},
+		{test_type::bic, {}, avalanche_mixer_bic_test<T>, "bic"},
 	};
 }
 
