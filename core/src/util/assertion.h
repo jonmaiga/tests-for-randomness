3455a1019e61b8bb898e1826cdc5aac2ec7b19d9
jonmaiga <jon@jonkagstrom.com>
2023-02-06 11:25:12 +0100

    - rename development define

--- core/src/util/assertion.h
+++ core/src/util/assertion.h
@@ -7,7 +7,7 @@ void assertion_2_impl(bool condition, const char* message1, const char* message2
 
 }
 
-#ifdef MIXER_DEVELOPMENT
+#ifdef TFR_DEVELOPMENT
 #   define assertion(expression, message) (tfr_internal::assertion_impl(expression, message, #expression, __FILE__, __LINE__))
 #   define assertion_2(expression, message1, message2) (tfr_internal::assertion_2_impl(expression, message1, message2, #expression, __FILE__, __LINE__))
 #else
