c97df2d2678c6f4ba0cf2a275176cdda0e1216a5
jonmaiga <jon@jonkagstrom.com>
2023-02-06 10:50:17 +0100

    - rename in assert

--- core/src/util/assertion.h
+++ core/src/util/assertion.h
@@ -8,8 +8,8 @@ void assertion_2_impl(bool condition, const char* message1, const char* message2
 }
 
 #ifdef MIXER_DEVELOPMENT
-#   define assertion(expression, message) (mixer_internal::assertion_impl(expression, message, #expression, __FILE__, __LINE__))
-#   define assertion_2(expression, message1, message2) (mixer_internal::assertion_2_impl(expression, message1, message2, #expression, __FILE__, __LINE__))
+#   define assertion(expression, message) (tfr_internal::assertion_impl(expression, message, #expression, __FILE__, __LINE__))
+#   define assertion_2(expression, message1, message2) (tfr_internal::assertion_2_impl(expression, message1, message2, #expression, __FILE__, __LINE__))
 #else
 #   define assertion(expression, message) (0);
 #   define assertion_2(expression, message1, message2) (0);
