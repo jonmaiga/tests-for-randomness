514747373990e8928378b10efc70760a7db7b500
jonmaiga <jon@jonkagstrom.com>
2023-02-14 10:39:29 +0100

    - test reports to a markdown file

---
+++ results/result_32.md
@@ -0,0 +1,28 @@
+# 32-bit results
+_Since TFR is new you should take the results with a grain of salt._
+
+Tests stop at 2^20 bytes.
+
+Source|TFR|
+-|-|
+mix32::xm3x|>20|
+mix32::xm2x|18|
+mix32::prospector|18|
+mix32::prospector\_boost|12|
+mix32::h2sql|13|
+mix32::murmur|12|
+mix32::xmx|10|
+mix32::wang\_1|10|
+mix32::jenkins|10|
+rng32::xm3x|>20|
+rng32::xm2x|>20|
+rng32::xmx|20|
+rng32::pcg\-1|>20|
+rng32::xoshiro128\+\+|>20|
+rng32::mt19937|>20|
+rng32::xorshift|16|
+rng32::minstd\_rand|10|
+combine32::xmx|10|
+combine32::xm2x|10|
+combine32::xm3x|>20|
+combine32::boost|10|
--- tool/src/command/test_command.h
+++ tool/src/command/test_command.h
@@ -11,56 +11,74 @@
 
 namespace tfr {
 
-inline auto create_result_callback(bool print_intermediate_results = true) {
-	return [print_intermediate_results](const test_battery_result& br, bool is_last) {
+using on_done_callback = std::function<void(test_battery_result, bool pass)>;
+
+inline auto create_result_callback(bool print_intermediate_results = true, const on_done_callback& on_done = {}) {
+	return [print_intermediate_results, on_done](const test_battery_result& br, bool is_last) {
 		bool proceed = !is_last;
 		bool pass = true;
-		if (proceed) {
-			if (const auto analysis = get_worst_statistic_analysis(br)) {
+		if (const auto analysis = get_worst_statistic_analysis(br)) {
+			if (!is_last) {
 				proceed = analysis->pass();
-				pass = analysis->pass();
 			}
+			pass = analysis->pass();
 		}
+
 		if (print_intermediate_results || !proceed) {
 			print_battery_result(br);
 		}
 		if (!proceed) {
-			std::ostringstream os;
-			os << br.test_subject_name << ";" << br.power_of_two() << ";" << (pass ? "PASS" : "FAIL") << "\n";
-			write_append(get_config().result_dir() + "result_" + std::to_string(br.bits) + ".txt", os.str());
+			if (on_done) {
+				on_done(br, pass);
+			}
 		}
 		return proceed;
 	};
 }
 
 inline void test_command() {
-	using T = uint32_t;
+	using T = uint64_t;
 
-	const auto callback = create_result_callback(false);
+	constexpr int max_power_of_two = 20;
+
+	const auto report_filename = get_config().test_result_file_path<T>();
+	std::ostringstream os;
+	os << "# " << bit_sizeof<T>() << "-bit results\n";
+	os << "_While TFR is new you should take the results with a grain of salt._\n\n";
+	os << "Tests stop at 2^" << max_power_of_two << " bytes.\n\n";
+	os << "Source|TFR|\n-|-|\n";
+	write(report_filename, os.str());
+	auto on_done_callback = [report_filename](const test_battery_result& br, bool pass) {
+		std::ostringstream os;
+		os << escape_for_md(br.test_subject_name) << "|" << (pass ? ">" : "") << br.power_of_two() << "|\n";
+		write_append(report_filename, os.str());
+	};
+
+	const auto callback = create_result_callback(false, on_done_callback);
 
 	// trng
 	if (const auto* data = get_trng_data<T>()) {
-		evaluate_multi_pass(callback, create_data_test_setup<T>("trng", *data));
+		evaluate_multi_pass(callback, create_data_test_setup<T>("trng", *data).range(10, max_power_of_two));
 	}
 
 	// drng
 	if (const auto* data = get_drng_data<T>()) {
-		evaluate_multi_pass(callback, create_data_test_setup<T>("drng", *data));
+		evaluate_multi_pass(callback, create_data_test_setup<T>("drng", *data).range(10, max_power_of_two));
 	}
 
 	// mixers
 	for (const auto& m : get_mixers<T>()) {
-		evaluate_multi_pass(callback, create_mixer_test_setup(m));
-	}
-
-	// combiners
-	for (const auto& combiner : get_combiners<T>()) {
-		evaluate_multi_pass(callback, create_combiner_test_setup<T>(combiner));
+		evaluate_multi_pass(callback, create_mixer_test_setup(m).range(10, max_power_of_two));
 	}
 
 	// prngs
 	for (const auto& prng : get_prngs<T>()) {
-		evaluate_multi_pass(callback, create_prng_test_setup<T>(prng));
+		evaluate_multi_pass(callback, create_prng_test_setup<T>(prng).range(10, max_power_of_two));
+	}
+
+	// combiners
+	for (const auto& combiner : get_combiners<T>()) {
+		evaluate_multi_pass(callback, create_combiner_test_setup<T>(combiner).range(10, max_power_of_two));
 	}
 }
 
