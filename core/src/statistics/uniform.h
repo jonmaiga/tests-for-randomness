a7f054efb56d6e6b1e5016e3da0d29264596d21a
jonmaiga <jon@jonkagstrom.com>
2023-11-27 15:15:59 +0100

    - enable pow2 binning to remove bias from uniform test.

--- core/src/statistics/uniform.h
+++ core/src/statistics/uniform.h
@@ -3,10 +3,12 @@
 #include "chi2.h"
 
 namespace tfr {
-
 template <typename T>
 sub_test_results uniform_test(const uint64_t n, const stream<T>& source) {
-	return main_sub_test(chi2_uniform_stats(ranged_stream(rescale_type_to_01(source), n)));
+	return main_sub_test(
+		chi2_uniform_stats(ranged_stream(rescale_type_to_01(source), n),
+		                   create_bin_count_pow2<T>()
+		)
+	);
 }
-
 }
