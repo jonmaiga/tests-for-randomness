fc19bc80369d84323c80170fa6a6afce458ab28e
jonmaiga <jon@jonkagstrom.com>
2023-11-28 11:08:46 +0100

    - added 8 bit test no change for divisibility.

--- unittest/src/statistics/divisibility_test.cpp
+++ unittest/src/statistics/divisibility_test.cpp
@@ -6,7 +6,6 @@
 
 
 namespace tfr {
-
 TEST(divisibility, collect_divisbility) {
 	using T = std::vector<uint64_t>;
 	EXPECT_EQ(collect_divisible<T>(2, 1, 4, {1,3,5,2,4}), T({1,0,0,1}));
@@ -30,4 +29,15 @@ TEST(divisibility, divisibility_no_change) {
 	EXPECT_NEAR(r->p_value, 0.6662, 1e-4);
 }
 
+TEST(divisibility, divisibility_no_change_8) {
+	auto s = test_stream();
+	std::vector<uint8_t> data;
+	for (int i = 0; i < (1<<22); ++i) {
+		data.push_back(static_cast<uint8_t>(s()));
+	}
+	const auto s8 = create_stream_from_data_by_ref("test", data);
+	auto r = divisibility_test<uint8_t>(data.size(), s8).front().stats;
+	EXPECT_NEAR(r->value, 11.1800, 1e-4);
+	EXPECT_NEAR(r->p_value, 0.7397, 1e-4);
+}
 }
