b139379d67d7e2b78d22bc9459edff555cc48741
jonmaiga <jon@jonkagstrom.com>
2023-12-03 09:39:22 +0100

    - updated 32-bit mixers with better constants.

--- impl/src/prngs32.h
+++ impl/src/prngs32.h
@@ -15,10 +15,10 @@ namespace tfr {
 using prng32 = prng<uint32_t>;
 
 namespace rng32 {
-inline prng32 xm3x(const seed_data& seed) {
+inline prng32 xmx(const seed_data& seed) {
 	return {
-		"rng32::xm3x", [state = seed.s32()]() mutable {
-			return state = mix32::xm3x(state);
+		"rng32::xmx", [state = seed.s32()]() mutable {
+			return state = mix32::mx1(state);
 		}
 	};
 }
@@ -26,32 +26,32 @@ inline prng32 xm3x(const seed_data& seed) {
 inline prng32 xm2x(const seed_data& seed) {
 	return {
 		"rng32::xm2x", [state = seed.s32()]() mutable {
-			return state = mix32::xm2x(state);
+			return state = mix32::mx2(state);
 		}
 	};
 }
 
-inline prng32 xmx(const seed_data& seed) {
+inline prng32 xm3x(const seed_data& seed) {
 	return {
-		"rng32::xmx", [state = seed.s32()]() mutable {
-			return state = mix32::xmx(state);
+		"rng32::xm3x", [state = seed.s32()]() mutable {
+			return state = mix32::mx3(state);
 		}
 	};
 }
 
-inline prng32 pcg(const seed_data& seed) {
+inline prng32 pcg_64(const seed_data& seed) {
 	pcg32 pcg(seed.s64());
 	return {
-		"rng32::pcg", [pcg]() mutable {
+		"rng32::pcg_64", [pcg]() mutable {
 			return pcg();
 		}
 	};
 }
 
-inline prng32 xoshiro128plusplus(const seed_data& seed) {
+inline prng32 xoshiro128plusplus_128(const seed_data& seed) {
 	// https://prng.di.unimi.it/xoshiro128plusplus.c
 	return {
-		"rng32::xoshiro128++", [s = seed.s128_32()]() mutable {
+		"rng32::xoshiro128++_128", [s = seed.s128_32()]() mutable {
 			const uint32_t result = rol(s[0] + s[3], 7) + s[0];
 			const uint32_t t = s[1] << 9;
 
@@ -118,9 +118,9 @@ inline prng32 aes(const seed_data& seed) {
 	};
 }
 
-inline prng32 aes128(const seed_data& seed) {
+inline prng32 aes_128(const seed_data& seed) {
 	return {
-		"rng32::aes128", [state = seed.s128_32()]() mutable {
+		"rng32::aes_128", [state = seed.s128_32()]() mutable {
 			return aes_prng128(state);
 		}
 	};
@@ -131,11 +131,11 @@ inline prng32 aes128(const seed_data& seed) {
 template <>
 inline std::vector<prng_factory<uint32_t>> get_prngs() {
 	return {
-		rng32::xm3x,
-		rng32::xm2x,
 		rng32::xmx,
-		rng32::pcg,
-		rng32::xoshiro128plusplus,
+		rng32::xm2x,
+		rng32::xm3x,
+		rng32::pcg_64,
+		rng32::xoshiro128plusplus_128,
 		rng32::mt19337,
 		rng32::xorshift,
 		rng32::minstd_rand,
