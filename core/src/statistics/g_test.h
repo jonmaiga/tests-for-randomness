d0e56a4cce586243635fea1652dfef49bb6863fd
jonmaiga <jon@jonkagstrom.com>
2024-01-13 09:21:07 +0100

    - Added g-test to be evaluated instead of chi2.

---
+++ core/src/statistics/g_test.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+#include "distributions.h"
+#include "util/algo.h"
+#include "util/math.h"
+
+namespace tfr {
+
+inline std::optional<statistic> g_test_stats(std::size_t n, const data_fn& observed, const data_fn& expected, double expected_threshold = 1) {
+	assertion(expected_threshold > 0, "G test requires expected threshold > 0");
+	double g = 0;
+	double df = 0;
+	for (std::size_t i = 0; i < n; ++i) {
+		const double expected_count = expected(i);
+		if (expected_count < expected_threshold) continue;
+		const double observed_count = observed(i);
+		const double s = observed_count * std::log(observed_count / expected_count);
+		g += s;
+		df += 1;
+	}
+	assertion(g >= 0, "The g test statistic is negative, make sure that total observed == total expected.");
+	df = std::max(df - 1, 0.);
+	if (is_near(df, 0)) {
+		return {};
+	}
+	g *= 2;
+	const auto p_value = chi2_distribution_cdf(g, df);
+	return statistic{statistic_type::chi2, g, p_value, df};
+}
+
+inline std::optional<statistic> g_test_stats(const std::vector<uint64_t>& bins, double expected_count, double expected_threshold = 1) {
+	return g_test_stats(bins.size(), to_data(bins), to_data(expected_count), expected_threshold);
+}
+
+}
---
+++ unittest/src/statistics/g_test_test.cpp
@@ -0,0 +1,20 @@
+#include <statistics/g_test.h>
+
+#include <gtest/gtest.h>
+
+#include "testutil.h"
+
+namespace tfr {
+
+TEST(g_test, basic) {
+	EXPECT_FALSE(g_test_stats({1}, 1));
+	EXPECT_NEAR(g_test_stats({1,1}, 1)->value, 0, 1e-4);
+	EXPECT_NEAR(g_test_stats({1,2}, 1)->value, 2.772589, 1e-4);
+	EXPECT_NEAR(g_test_stats({80,125,95}, 100)->value, 10.337194, 1e-4);
+
+	using V = std::vector<uint64_t>;
+	EXPECT_NEAR(g_test_stats(2, to_data(V{1,2}), to_data(V{1,2}))->value, 0, 1e-4);
+	EXPECT_NEAR(g_test_stats(3, to_data(V{80,125,95}), to_data(V{94,112,94}))->value, 3.661414, 1e-4);
+}
+
+}
