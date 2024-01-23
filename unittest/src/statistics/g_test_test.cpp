ef29be8a4bb1bd763afc262bbaeb4f066ebf48e1
jonmaiga <jon@jonkagstrom.com>
2024-01-23 11:11:53 +0100

    - Rescaling g so we get expected == observed total count

--- unittest/src/statistics/g_test_test.cpp
+++ unittest/src/statistics/g_test_test.cpp
@@ -9,7 +9,7 @@ namespace tfr {
 TEST(g_test, basic) {
 	EXPECT_FALSE(g_test_stats({1}, 1));
 	EXPECT_NEAR(g_test_stats({1,1}, 1)->value, 0, 1e-4);
-	EXPECT_NEAR(g_test_stats({1,2}, 1)->value, 2.772589, 1e-4);
+	EXPECT_NEAR(g_test_stats({1,2}, 1)->value, 0.3397, 1e-4);
 	EXPECT_NEAR(g_test_stats({80,125,95}, 100)->value, 10.337194, 1e-4);
 
 	using V = std::vector<uint64_t>;
